7c9c396d9821aa8707c3380dbc2ac7a72fc33aa1
haicover <120477508+haicover@users.noreply.github.com>
2026-05-19 22:28:57 +0700

    Khởi tạo KMP template: Tạo file build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+
+plugins {
+    kotlin("multiplatform") version "1.9.20" apply false
+    kotlin("serialization") version "1.9.20" apply false
+    id("com.android.application") version "8.1.2" apply false
+    id("com.android.library") version "8.1.2" apply false
+}
+        
